1760e39ef76c12d94637396e3cb64f444f915a7a
Mohamed Waseem <waseemmd907@gmail.com>
2026-09-22 14:54:18 +0530

    Change floating launcher to navigate directly to CRM tab instead of modal popup

--- salesforce/force-app/main/default/lwc/whatzuppFloatingLauncher/whatzuppFloatingLauncher.js
+++ salesforce/force-app/main/default/lwc/whatzuppFloatingLauncher/whatzuppFloatingLauncher.js
@@ -1,25 +1,13 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class WhatzuppFloatingLauncher extends NavigationMixin(LightningElement) {
-    @api launchMode = 'Modal'; // Configurable via App Builder: 'Modal' or 'Tab'
-    
-    @track showModal = false;
-
     handleLaunch() {
-        if (this.launchMode === 'Tab') {
-            this[NavigationMixin.Navigate]({
-                type: 'standard__navItemPage',
-                attributes: {
-                    apiName: 'WhatZupp_CRM'
-                }
-            });
-        } else {
-            this.showModal = true;
-        }
-    }
-
-    handleClose() {
-        this.showModal = false;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__navItemPage',
+            attributes: {
+                apiName: 'WhatZupp_CRM'
+            }
+        });
     }
 }
